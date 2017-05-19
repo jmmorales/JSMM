d01726cb67ab8aee9c0dc2b116d0179c8e216fa8
daniellectlr <daniellectlr@gmail.com>
2017-05-19 16:06:09 +0100

    transformed to function

--- R/trait_matrix.R
+++ R/trait_matrix.R
@@ -1,15 +1,27 @@
-### Build traits matrix with columns of 0 and 1 for cathegorical traits and numerical column for continuous variables ###
+### Build traits matrix with columns of 0 and 1 for categorical traits and numerical column for continuous variables ###
 
 #input:
   #ns - number of species
   #nt - number of traits
   #transformation for continuous variable
 
-T = matrix(0,nrow=ns, ncol=nt)
-
-colpos = di1
-for(i in 1:ns){
-  T[i,colpos[i]] =1 
-} # T matrix does not include the intercept column (tk1=1)
-
-T[,length(traits)] = log(ma1)
+Tmatrix <- function(..., ns = ns, nt = nt, apply.log = TRUE){
+  
+  T <- matrix(0,nrow=ns, ncol=nt)
+  x <- list(...)
+  n <- length(x)
+  for(i in 1:n){
+    if(is.character(x[[i]])){
+      colpos = x[[i]]
+      for(j in 1:ns){
+        T[j,colpos[j]] = 1 # T matrix does not include the intercept column (tk1=1)
+      } 
+    } else {
+      if(apply.log == TRUE) T[,length(traits)] = log(x[[i]])
+      else T[,length(traits)] = x[[i]]
+    }
+  }
+  
+  return(T)
+  
+}
