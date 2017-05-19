954951e785441b4f12296f0a99d4fe28163e6cbb
daniellectlr <daniellectlr@gmail.com>
2017-05-19 18:16:38 +0100

    transformed to function - version 1

--- R/checktracks.R
+++ R/checktracks.R
@@ -1,54 +1,82 @@
 ### Delete species from the tracks table not present in the atrib table or with no traits values 
-  #or not in the corr matrix ###
+  # or not in the corr matrix ### Delete species from the atributes and phylogenetic tables not 
+  # present in the tracks table
 
-#input:
-  # matrix of tracks; columns: specie/track ID/sequence of the point/position (pixel number)
-  # correlation table
-  # traits table (first column are species names; other columns are numerical ou cathegorical traits)
-
-TA1 = NA #tracks with more than 1 point
-SPS1 = NA #species identification for tracks
-#SP1 = NA
-TID1 = NA #track ID
-ma1 = NA
-di1 = NA
-cindex = NA
-SP = NA
-prob1 = prob2 = prob3 = NA #problems found
-
-for(i in 1:ns){
-  po = tsp[tsp==sps[i]]
-  poc = which(csp==sps[i])
-  if(length(po)==0) prob1 = c(prob1,sps[i])
-  if(length(poc)==0) prob3 = c(prob3,sps[i])
-  if(length(po)!=0 & length(poc)!=0) {
-    lma = mass[tsp==sps[i]]
-    if(is.numeric(lma)) {
-      TA1 = c(TA1,TA[SPS==sps[i]])
-      cindex = c(cindex, poc[1])
-      SPS1 = c(SPS1,SPS[SPS==sps[i]])
-      TID1 = c(TID1, TID[SPS==sps[i]])
-      #SP1 = c(SP1,sps[i])
-      ma1 = c(ma1,lma)
-      di1 = c(di1,diet[tsp==sps[i]])
-      SP = c(SP,sps[i])
+# input:
+  # matrix of tracks; columns: specie/track ID/position (pixel number); the position should be 
+    # in the correct order of movement and the order of the lines represent the order of movement
+  # phylogenetic correlation table
+  # traits table (first column are species names; other columns are numerical ou cathegorical 
+    # traits)
+  # requires one continuous variable (mass)
+  
+set.input <- function (tracks = tracks, traits = traits, phy.cor = cor, body.mass = traits[,11]){
+  
+  # check if tracks table has 3 columns
+  if(!ncol(tracks = 3)) {
+    stop("Error: the table with tracks should have 3 columns")
+  } else {
+    # check if 1 column is cathegorical (species names), and if other columns are numerical 
+      # (track ID/position (pixel number))
+    if(!is.factor(tracks[,1])|!is.numeric(tracks[,2])|!is.numeric(tracks[,3])){
+      stop("Error: the first column of the table with tracks should contain the species 
+identification, the second the tracks identification, and the third the position")
+    } else {
+      # check if tlength is integer
+      if(!is.integer(tlength)){
+        stop("Error: tlength should be numerical")
+      } else{
+        
+        TA1 = NA #tracks with more than 1 point
+        SPS1 = NA #species identification for tracks
+        #SP1 = NA
+        TID1 = NA #track ID
+        traits1 = traits[-c(1:nrow(traits)),]
+        #ma1 = NA
+        #di1 = NA
+        cindex = NA
+        SP = NA
+        prob1 = prob2 = prob3 = NA #problems found
+        
+        sps <- unique(tracks[,1])
+        ns <- length(sps)
+        csp <- as.vector(unlist(cor[1,]))
+        mass <- body.mass
+        
+        for(i in 1:ns){
+          po = traits[,1][traits[,1]==sps[i]]
+          poc = which(csp==sps[i])
+          if(length(po)==0) prob1 = c(prob1,sps[i])
+          if(length(poc)==0) prob3 = c(prob3,sps[i])
+          if(length(po)!=0 & length(poc)!=0) {
+            lma = mass[traits[,1]==sps[i]]
+            if(is.numeric(lma)) {
+              TA1 = c(TA1,tracks[,3][tracks[,1]==sps[i]])
+              cindex = c(cindex, poc[1])
+              SPS1 = c(SPS1,tracks[,1][tracks[,1]==sps[i]])
+              TID1 = c(TID1, tracks[,2][tracks[,1]==sps[i]])
+              #SP1 = c(SP1,sps[i])
+              traits1 = rbind.data.frame(traits1,traits[mass[mass==lma],])
+              SP = c(SP,sps[i])
+            }
+            
+            else prob2 = c(prob2,sps[i])
+          }
+        }
+        print(paste("species with no traits: ", prob1))
+        print(paste("species with no trait values: ", prob2))
+        print(paste("species with no phylogeny: ", prob3))
+        
+        TA = TA1[!is.na(TA1)] #sÃ³ tracks com pelo menos 2 pontos
+        #SP = SP1[!is.na(SP1)] #espÃ©cies observadas
+        TID = TID1[!is.na(TID1)]
+        SPS = SPS1[!is.na(SPS1)]
+        SP = SP[!is.na(SP)]
+        cindex = cindex[!is.na(cindex)]
+        CC= cor[cindex,cindex] #same order for species in the correlation table
+        
+        return(list(SP, tracks = data.frame(SPS,TID,TA), traits = traits1, cor = CC))
+      }
     }
-    
-    else prob2 = c(prob2,sps[i])
   }
 }
-print(paste("species with no traits: ", prob1))
-print(paste("species with no trait values: ", prob2))
-print(paste("species with no phylogeny: ", prob3))
-
-TA = TA1[!is.na(TA1)] #só tracks com pelo menos 2 pontos
-#SP = SP1[!is.na(SP1)] #espécies observadas
-TID = TID1[!is.na(TID1)]
-SPS = SPS1[!is.na(SPS1)]
-ma1 = ma1[!is.na(ma1)] #vetor de pesos por espécie (length=ns)
-di1 = di1[!is.na(di1)]
-SP = SP[!is.na(SP)]
-cindex = cindex[!is.na(cindex)]
-CC= cor[cindex,cindex] #same order for species in the correlation table
-
-####end#####
